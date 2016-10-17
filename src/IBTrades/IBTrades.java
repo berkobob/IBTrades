dc2d2d271b066b27eae5918e0a6a231006c83545
berkobob <alever@omniis.com>
2016-10-17 09:13:37 +0100

    Add a comment about why have IBTrades.java

--- src/IBTrades/IBTrades.java
+++ src/IBTrades/IBTrades.java
@@ -9,6 +9,8 @@ public class IBTrades {
 
 	public static void main(String[] args) {
 		
+		/* I think I created this to see if all the trades should be in an object */
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				IBTradesView view = new IBTradesView();
